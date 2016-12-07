c20786f03dec118eb088b7add6da012b58026019
Elvis Henrique Pereira <elvishp2006@gmail.com>
2016-12-07 16:42:48 -0200

    Using new lib on widgets

--- Widget/implement-widget.php
+++ Widget/implement-widget.php
@@ -2,33 +2,25 @@
 
 namespace GB\API;
 
-// Avoid that files are directly loaded
 if ( ! function_exists( 'add_action' ) ) {
 	exit( 0 );
 }
 
-use WP_Widget;
 use ReflectionClass;
+use Carbon_Fields\Widget;
 
-class Implement_Widget extends WP_Widget
+class Implement_Widget extends Widget
 {
 	protected function get_property( $instance, $property, $default = '' )
 	{
 		return ( isset( $instance[ $property ] ) && ! empty( $instance[ $property ] ) ) ? $instance[ $property ] : $default;
 	}
 
-	/**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
-	public function widget( $args, $instance )
+	public function front_end( $args, $instance )
 	{
-		$file_name = str_replace( '_', '-', strtolower( (new ReflectionClass( $this ) )->getShortName() ) ) . '.php';
+		$name = ( new ReflectionClass( $this ) )->getShortName();
+		$name = str_replace( '_', '-', strtolower( $name ) );
 
-		include TEMPLATEPATH . '/widgets/' . $file_name;
+		include TEMPLATEPATH . "/widgets/{$name}.php";
 	}
 }
