ecdb80c02c0aa9859e82ca040ad2ca6a69dba1eb
Elvis Henrique Pereira <elvishp2006@gmail.com>
2016-12-08 13:17:10 -0200

    Refactoring user class

--- Model/user.php
+++ Model/user.php
@@ -2,12 +2,10 @@
 
 namespace GB\API;
 
-// Avoid that files are directly loaded
 if ( ! function_exists( 'add_action' ) ) {
 	exit( 0 );
 }
 
-use Exception;
 use stdClass;
 
 abstract class User
@@ -92,141 +90,141 @@ abstract class User
 	 */
 	private $url;
 
-    /**
+	/**
 	 * Admin Color
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $admin_color;
+	private $admin_color;
 
-    /**
+	/**
 	 * Aim
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $aim;
+	private $aim;
 
-    /**
+	/**
 	 * Comment Shortcuts
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $comment_shortcuts;
+	private $comment_shortcuts;
 
-    /**
+	/**
 	 * Description
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $description;
+	private $description;
 
-    /**
+	/**
 	 * Display Name
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $display_name;
+	private $display_name;
 
-    /**
+	/**
 	 * First Name
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $first_name;
+	private $first_name;
 
-    /**
+	/**
 	 * Last Name
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $last_name;
+	private $last_name;
 
-    /**
+	/**
 	 * Google Plus
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $googleplus;
+	private $googleplus;
 
-    /**
+	/**
 	 * Jabber
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $jabber;
+	private $jabber;
 
-    /**
+	/**
 	 * Level
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $level;
+	private $level;
 
-    /**
+	/**
 	 * Nickname
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $nickname;
+	private $nickname;
 
-    /**
+	/**
 	 * Plugins Last View
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $plugins_last_view;
+	private $plugins_last_view;
 
-    /**
+	/**
 	 * Plugins Per Page
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $plugins_per_page;
+	private $plugins_per_page;
 
-    /**
+	/**
 	 * Rich Editing
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $rich_editing;
+	private $rich_editing;
 
-    /**
+	/**
 	 * Roles
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $roles;
+	private $roles;
 
-    /**
+	/**
 	 * Twitter
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $twitter;
+	private $twitter;
 
-    /**
+	/**
 	 * Yim
 	 *
 	 * @since 1.0
 	 * @var string
 	 */
-    private $yim;
+	private $yim;
 
 	/**
 	 * Use in fields user has "user_" prefix
@@ -242,8 +240,8 @@ abstract class User
 		'pass',
 		'registered',
 		'status',
-        'level',
-        'url',
+		'level',
+		'url',
 	);
 
 	/**
@@ -254,34 +252,34 @@ abstract class User
 	 */
 	private $literal_user_fields = array(
 		'admin_color',
-        'aim',
-        'comment_shortcuts',
-        'description',
-        'display_name',
-        'first_name',
-        'googleplus',
-        'jabber',
-        'last_name',
-        'nickname',
-        'plugins_last_view',
-        'plugins_per_page',
-        'rich_editing',
-        'roles',
-        'twitter',
+		'aim',
+		'comment_shortcuts',
+		'description',
+		'display_name',
+		'first_name',
+		'googleplus',
+		'jabber',
+		'last_name',
+		'nickname',
+		'plugins_last_view',
+		'plugins_per_page',
+		'rich_editing',
+		'roles',
+		'twitter',
 	);
 
 	/**
-     * Constructor of the class. Instantiate and incializate it.
-     *
-     * @since 1.0.0
-     *
-     * @param int $ID - The ID of the Customer
-     * @return null
-     */
-	public function __construct( $ID = false )
+	 * Constructor of the class. Instantiate and incializate it.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $ID - The ID of the Customer
+	 * @return null
+	 */
+	public function __construct( $id = false )
 	{
-		if ( false != $ID ) :
-			$this->ID = $ID;
+		if ( false !== $id ) :
+			$this->ID = $id;
 		endif;
 	}
 
@@ -294,17 +292,18 @@ abstract class User
 		return $this->parse( Utils::get_user_query( $args, $defaults ) );
 	}
 
-    public function parse( $wp_user_query )
+	public function parse( $wp_user_query )
 	{
-		if ( ! $wp_user_query->results )
+		if ( ! $wp_user_query->results ) {
 			return false;
+		}
 
-		foreach ( $wp_user_query->results as $id ) :
+		foreach ( $wp_user_query->results as $id ) {
 			$model  = new $this( $id );
 			$list[] = $model;
 
 			unset( $model );
-		endforeach;
+		}
 
 		$std                = new stdClass();
 		$std->list          = $list;
@@ -315,18 +314,19 @@ abstract class User
 
 	public function __get( $prop_name )
 	{
-		if ( isset( $this->$prop_name ) )
+		if ( isset( $this->$prop_name ) ) {
 			return $this->$prop_name;
+		}
 
-		if ( in_array( $prop_name, $this->prefix_user_fields ) ) :
+		if ( in_array( $prop_name, $this->prefix_user_fields, true ) ) {
 			$this->$prop_name = get_the_author_meta( "user_{$prop_name}", $this->ID );
 			return $this->$prop_name;
-		endif;
+		}
 
-		if ( in_array( $prop_name, $this->literal_user_fields ) ) :
+		if ( in_array( $prop_name, $this->literal_user_fields, true ) ) {
 			$this->$prop_name = get_the_author_meta( $prop_name, $this->ID );
 			return $this->$prop_name;
-		endif;
+		}
 
 		if ( array_key_exists( $prop_name, $this->metas ) ) :
 			$this->$prop_name = $this->get_meta_value( $prop_name );
@@ -338,40 +338,18 @@ abstract class User
 
 	public function get_meta_value( $meta_key )
 	{
-		$args  = wp_parse_args( $this->metas[$meta_key], array( 'single' => true ) );
-		$value = get_user_meta( $this->ID, $meta_key, $args['single'] );
+		$args  = $this->metas[ $meta_key ];
+		$value = carbon_get_user_meta( $this->ID, $meta_key, @$args['type'] );
 
-		if ( isset( $args['default'] ) && empty( $value ) )
-			return $args['default'];
+		if ( ! $value ) {
+			return @$args['default'];
+		}
 
-		if ( isset( $args['sanitize'] ) && is_callable( $args['sanitize'] ) )
+		if ( @$args['sanitize'] && is_callable( $args['sanitize'] ) ) {
 			return call_user_func( $args['sanitize'], $value );
+		}
 
 		return $value;
-	}
-
-	public function get_meta_name( $meta_key )
-	{
-		if ( ! array_key_exists( $meta_key, $this->metas ) )
-			throw new Exception( 'the meta_key passed is not defined', 100 );
-
-		return $meta_key;
-	}
-
-	public function set_meta( $meta_key, $value )
-	{
-		if ( ! isset( $this->ID ) )
-			return false;
-
-		update_user_meta( $this->ID, $meta_key, $value );
-	}
-
-	public function get_meta( $meta_key, $single = true )
-	{
-		if ( ! isset( $this->ID ) )
-			return false;
-
-		return get_user_meta( $this->ID, $meta_key, $single );
 	}
 
 	/**
