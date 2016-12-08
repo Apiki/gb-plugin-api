476ea015c9612dac1ed33c6e585db0b659edae09
Elvis Henrique Pereira <elvishp2006@gmail.com>
2016-12-08 12:57:12 -0200

    Create users abstract

--- Controller/index.php
+++ Controller/index.php
@@ -2,7 +2,6 @@
 
 namespace GB\API;
 
-// Avoid that files are directly loaded
 if ( ! function_exists( 'add_action' ) ) {
 	exit( 0 );
 }
@@ -10,4 +9,5 @@ if ( ! function_exists( 'add_action' ) ) {
 App::uses( 'Controller', 'post-type' );
 App::uses( 'Controller', 'taxonomy' );
 App::uses( 'Controller', 'widget' );
+App::uses( 'Controller', 'users' );
 
