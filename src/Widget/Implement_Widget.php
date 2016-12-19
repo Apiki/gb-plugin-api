9c010a04300615ceebfb3e937b6684f10d2ae035
kassyn <kassyntec@gmail.com>
2016-12-19 16:21:59 -0200

    document attr and methods class Implement_Widget

--- src/Widget/Implement_Widget.php
+++ src/Widget/Implement_Widget.php
@@ -11,8 +11,19 @@ use Carbon_Fields\Widget;
 
 class Implement_Widget extends Widget
 {
+	/**
+	 * Instance fields with props.
+	 *
+	 * @var array
+	 */
 	private $instance;
 
+	/**
+	 * Get prop by instance.
+	 *
+	 * @param string $prop name of prop.
+	 * @return mixed value of instance by index.
+	 */
 	protected function get_prop( $prop, $default = '' )
 	{
 		if ( ! isset( $this->instance[ $prop ] ) ) {
@@ -22,6 +33,14 @@ class Implement_Widget extends Widget
 		return ( $this->instance[ $prop ] ) ? $this->instance[ $prop ] : $default;
 	}
 
+	/**
+	 * Used for html front-end in theme.
+	 * To work is need to create an file in theme.
+	 * Like class name Box_Tags_Widget create file in widgets folder box-tags-widget.php.
+	 *
+	 * @param array $args
+	 * @return array $instance
+	 */
 	public function front_end( $args, $instance )
 	{
 		$name = ( new ReflectionClass( $this ) )->getShortName();
