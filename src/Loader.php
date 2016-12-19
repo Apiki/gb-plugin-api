5959cb2e7217feb4908dc79a12302244752dfe6d
kassyn <kassyntec@gmail.com>
2016-12-19 15:25:16 -0200

    document attr and methods class Loader

--- src/Loader.php
+++ src/Loader.php
@@ -11,10 +11,9 @@ use ReflectionClass;
 abstract class Loader
 {
 	/**
-	 * Pages Enqueue Media
+	 * The pages where must print script.
 	 *
-	 * @since 1.1
-	 * @var array
+	 * @var string
 	 */
 	public $pages_enqueue_media = array(
 		'post.php',
@@ -22,8 +21,18 @@ abstract class Loader
 		'themes.php',
 	);
 
+	/**
+	 * Base path used for include files.
+	 *
+	 * @var string
+	 */
 	protected static $root_file;
 
+	/**
+	 * SLUG plugin.
+	 *
+	 * @var string
+	 */
 	const SLUG = 'gb-plugin-api';
 
 	public function __construct( $file = false )
@@ -37,16 +46,28 @@ abstract class Loader
 		$this->initialize();
 	}
 
+	/**
+	 * Initialize.
+	 */
 	public function initialize()
 	{
 
 	}
 
+	/**
+	 * Load textdomain.
+	 */
 	public function load_textdomain()
 	{
 		load_plugin_textdomain( static::SLUG, false, self::plugin_rel_path( 'languages' ) );
 	}
 
+	/**
+	 * Prepare the item for the REST response.
+	 *
+	 * @param array $controllers list of controllers.
+	 * @param bool $activate used for add capabilities in creating instance.
+	 */
 	public function load_controllers( $controllers, $activate = false )
 	{
 		$namespace = $this->get_namespace();
@@ -56,11 +77,19 @@ abstract class Loader
 		}
 	}
 
+	/**
+	 * Get namespace this class.
+	 *
+	 * @return string
+	 */
 	public function get_namespace()
 	{
 		return ( new ReflectionClass( $this ) )->getNamespaceName();
 	}
 
+	/**
+	 * Enqueue script media wp.media.
+	 */
 	public function load_wp_media()
 	{
 		global $pagenow;
@@ -74,41 +103,82 @@ abstract class Loader
 		}
 	}
 
+	/**
+	 * Scripts for admin WordPress
+	 */
 	public function scripts_admin()
 	{
 
 	}
 
+	/**
+	 * Styles for admin WordPress
+	 */
 	public function styles_admin()
 	{
 
 	}
 
+	/**
+	 * Get root file per static::SLUG
+	 *
+	 * @return string
+	 */
 	public static function get_root_file()
 	{
 		return self::$root_file[ static::SLUG ];
 	}
 
+	/**
+	 * Get absolute path.
+	 *
+	 * @param string $path
+	 * @return string
+	 */
 	public static function plugin_dir_path( $path )
 	{
 		return plugin_dir_path( self::get_root_file() ) . $path;
 	}
 
+	/**
+	 * Get path base plugin.
+	 *
+	 * @param string $path
+	 * @return string
+	 */
 	public static function plugin_rel_path( $path )
 	{
-		return dirname( plugin_basename( self::get_root_file() ) ) . '/languages';
+		return dirname( plugin_basename( self::get_root_file() ) ) . '/' . $path;
 	}
 
+	/**
+	 * Get plugin url with path of increment.
+	 *
+	 * @param string $path
+	 * @return string
+	 */
 	public static function plugins_url( $path )
 	{
 		return plugins_url( $path, self::get_root_file() );
 	}
 
+	/**
+	 * Get filemtime
+	 *
+	 * @param string $path
+	 * @return string
+	 */
 	public static function filemtime( $path )
 	{
 		return filemtime( self::plugin_dir_path( $path ) );
 	}
 
+	/**
+	 * Cretae instance of controllers
+	 *
+	 * @param string $class class name.
+	 * @param bool $activate used for add capabilities in creating instance.
+	 */
 	private function _handle_instance( $class, $activate = false )
 	{
 		$instance = new $class( $activate );
