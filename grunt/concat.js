a8e8609d13e5ae62ec15d38f8d73c5501db2087d
kassyn <kassyntec@gmail.com>
2017-02-02 17:41:26 -0200

    add new work tasks for grunt

--- grunt/concat.js
+++ grunt/concat.js
@@ -2,13 +2,12 @@ module.exports = {
 	options : {
 		separator : ';'
 	},
-	dist : {
+	dev : {
 		src : [
 			'<%= paths.js %>/libs/*.js',
 			'<%= paths.js %>/templates/*.js',
 			'<%= paths.js %>/vendor/*.js',
-			'<%= paths.js %>/custom/*.js',
-			'<%= paths.js %>/boot.js'
+			'<%= paths.js %>/app/*.js'
 		],
 		dest : '<%= paths.js %>/built.js',
 	}
