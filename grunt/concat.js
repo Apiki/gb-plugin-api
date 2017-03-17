52fefad50d19fdfcda64cfbeab413c9ca0482e06
kassyn <kassyntec@gmail.com>
2017-03-17 18:03:51 -0300

    add boot.js in concat files javascripts

--- grunt/concat.js
+++ grunt/concat.js
@@ -7,7 +7,8 @@ module.exports = {
 			'<%= paths.js %>/libs/*.js',
 			'<%= paths.js %>/templates/*.js',
 			'<%= paths.js %>/vendor/*.js',
-			'<%= paths.js %>/app/*.js'
+			'<%= paths.js %>/app/*.js',
+			'<%= paths.js %>/boot.js'
 		],
 		dest : '<%= paths.js %>/built.js',
 	}
